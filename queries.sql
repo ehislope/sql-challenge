108b7d5f0f1a04dcc19d8dce6086da03a9f451b7
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-08-22 15:51:11 -0700

    updating queries.sql

--- queries.sql
+++ queries.sql
@@ -11,18 +11,37 @@ FROM employees
 LEFT JOIN salaries ON
 employees.emp_no = salaries.emp_no
 ORDER BY emp_no ASC;
---List first name, last name, and hire date for employees who were hired in 1986. - all on employees table
+
+--List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%/%/1986';
+
 --List the manager of each department with the following information: 
 	--department number, department name, the manager's employee number, last name, first name. 
-	-- dept_manager, departments, employees
-	--use joins department manager and departments on dept_no and then join employee info on employee id - inner joins
 	
+SELECT departments.dept_no,
+  departments.dept_name,
+  dept_manager.emp_no,
+  employees.last_name,
+  employees.first_name
+FROM dept_manager
+INNER JOIN departments ON
+dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON
+dept_manager.emp_no = employees.emp_no;
+
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
-	--join employee to dept_emp by employee_no and then join departments by dept_no. -inner join. Order by employee number
-	
+SELECT employees.emp_no,
+ employees.last_name,
+ employees.first_name,
+ departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON
+employees.emp_no = dept_emp.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no = departments.dept_no
+ORDER BY emp_no;
 --List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B." 
 -- use Where to find first name and LIKE to find last name - all in employee table.
 
